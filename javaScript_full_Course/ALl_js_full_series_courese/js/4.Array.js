82cd94293faeff7af3667fbf63d8cb90aa739e0c
debitkatuwal96-websitedeveloper <katuwalroshan790@gmail.com>
2025-03-24 08:39:37 -0500

    all files changes

---
+++ javaScript_full_Course/ALl_js_full_series_courese/js/4.Array.js
@@ -0,0 +1,38 @@
+
+// Array in javaScript
+// Array is a collection of elements
+// Array can store multiple values in a single variable
+// Array can store different data types
+// Array can store same data types
+// Array can store objects
+// Array can store functions
+// Array can store arrays
+// Array can store arrays of objects
+// Array can store arrays of functions
+// Array can store arrays of arrays
+
+let heros =
+    [
+        "Superman",
+        "Batman",
+        "ironman"
+    ];
+console.log(heros);
+
+let marks =
+    [
+        1,
+        2,
+        3,
+        4,
+    
+
+    ];
+console.log(marks);
+
+let info = [
+    "Rahul",
+    25,
+    "Delhi"
+];
+console.log(info);
